afaaa3c9b0b9aaa17c3132669820e4dee6ba2fb0
matewilk <matewilk@gmail.com>
2023-04-17 15:13:49 +0100

    (feat): add few more basic commands

--- packages/code-export-js-newrelic-synthetics/src/command.ts
+++ packages/code-export-js-newrelic-synthetics/src/command.ts
@@ -7,28 +7,7 @@ import {
 import { CommandShape } from '@seleniumhq/side-model'
 import location from './location'
 
-async function emitClick(target: string) {
-  return Promise.resolve(
-    `await $webDrive.findElement(${await location.emit(target)}).click()`
-  )
-}
-
-function variableLookup(varName: string) {
-  return `vars["${varName}"]`
-}
-
-async function emitNewWindowHandling(
-  command: CommandShape,
-  emittedCommand: ExportFlexCommandShape
-) {
-  return Promise.resolve(
-    `vars["windowHandles"] = await driver.getAllWindowHandles()\n${await emittedCommand}\nvars["${
-      command.windowHandleName
-    }"] = await waitForWindow(${command.windowTimeout})`
-  )
-}
-
-function emitWaitForWindow() {
+const emitWaitForWindow = async () => {
   const generateMethodDeclaration = (name: string) => {
     return {
       body: `async function ${name}(timeout = 2) {`,
@@ -61,6 +40,139 @@ function emitWaitForWindow() {
   })
 }
 
+const emitNewWindowHandling = async (
+  command: CommandShape,
+  emittedCommand: ExportFlexCommandShape
+) =>
+  Promise.resolve(
+    `vars["windowHandles"] = await driver.getAllWindowHandles()\n${await emittedCommand}\nvars["${
+      command.windowHandleName
+    }"] = await waitForWindow(${command.windowTimeout})`
+  )
+
+const emitAssert = async (varName: string, value: string) =>
+  Promise.resolve(`assert(vars["${varName}"].toString() == "${value}")`)
+
+const emitAssertAlert = async (alertText: string) =>
+  Promise.resolve(
+    `assert(await $webDriver.switchTo().alert().getText() == "${alertText}")`
+  )
+
+const emitAnswerOnNextPrompt = async (answer: string) => {
+  const commands = [
+    {
+      level: 0,
+      statement: 'const alert = await $webDriver.switchTo().alert()',
+    },
+    { level: 0, statement: `await alert().sendKeys("${answer}")` },
+    { level: 0, statement: 'await alert().accept()' },
+  ]
+  return Promise.resolve({ commands })
+}
+
+const emitCheck = async (locator: string) => {
+  const commands = [
+    {
+      level: 0,
+      statement: `const element = await $webDriver.wait(until.elementLocated(${await location.emit(
+        locator
+      )}))`,
+    },
+    {
+      level: 0,
+      statement: `if(!await element.isSelected()) await element.click()`,
+    },
+  ]
+
+  return Promise.resolve({ commands })
+}
+
+const emitChooseCancelOnNextConfirmation = async () =>
+  Promise.resolve(`await $webDriver.switchTo().alert().dismiss()`)
+
+const emitChooseOkOnNextConfirmation = async () =>
+  Promise.resolve(`await $webDriver.switchTo().alert().accept()`)
+
+const emitClick = async (target: string) =>
+  Promise.resolve(
+    `await $webDriver.wait(until.elementLocated(${await location.emit(
+      target
+    )})).click()`
+  )
+
+const emitClose = async () => Promise.resolve(`await $webDriver.close()`)
+
+const emitDragAndDrop = async (dragged: string, dropped: string) => {
+  const commands = [
+    {
+      level: 0,
+      statement: `const dragged = await $webDriver.wait(until.elementLocated(${await location.emit(
+        dragged
+      )}))`,
+    },
+    {
+      level: 0,
+      statement: `const dropped = await $webDriver.wait(until.elementLocated(${await location.emit(
+        dropped
+      )}))`,
+    },
+    {
+      level: 0,
+      statement:
+        'await $webDriver.actions().dragAndDrop(dragged, dropped).perform()',
+    },
+  ]
+  return Promise.resolve({ commands })
+}
+
+const emitEcho = (message: string) => {
+  const _message = message.startsWith('vars[') ? message : `"${message}"`
+  return Promise.resolve(`console.log(${_message})`)
+}
+
+const emitOpen = async (target: string) => {
+  const url = /^(file|http|https):\/\//.test(target)
+    ? `"${target}"`
+    : // @ts-expect-error globals yuck
+      `"${global.baseUrl}${target}"`
+  return Promise.resolve(`await $webDriver.get(${url})`)
+}
+
+const generateSendKeysInput = (value: string | string[]) => {
+  if (typeof value === 'object') {
+    return value
+      .map((s) => {
+        if (s.startsWith('vars[')) {
+          return s
+        } else if (s.startsWith('Key[')) {
+          const key = s.match(/\['(.*)'\]/)?.[1]
+          return `Key.${key}`
+        } else {
+          return `"${s}"`
+        }
+      })
+      .join(', ')
+  } else {
+    if (value.startsWith('vars[')) {
+      return value
+    } else {
+      return `"${value}"`
+    }
+  }
+}
+
+const emitType = async (target: string, value: string) => {
+  return Promise.resolve(
+    `await $webDriver.wait(until.elementLocated(${await location.emit(
+      target
+    )})).sendKeys(${generateSendKeysInput(value)})`
+  )
+}
+
+const variableLookup = (varName: string) => {
+  return `vars["${varName}"]`
+}
+
 function emit(command: CommandShape) {
   return exporter.emit.command(command, emitters[command.command], {
     variableLookup,
@@ -68,8 +180,27 @@ function emit(command: CommandShape) {
   })
 }
 
+const skip = async () => Promise.resolve('')
+
 export const emitters: Record<string, ProcessedCommandEmitter> = {
+  assert: emitAssert,
+  assertAlert: emitAssertAlert,
+  check: emitCheck,
+  chooseCancelOnNextConfirmation: skip,
+  chooseCancelOnNextPrompt: skip,
+  chooseOkOnNextConfirmation: skip,
+  type: emitType,
+  open: emitOpen,
   click: emitClick,
+  clickAt: emitClick,
+  close: emitClose,
+  debugger: skip,
+  dragAndDropToObject: emitDragAndDrop,
+  echo: emitEcho,
+  webdriverAnswerOnVisiblePrompt: emitAnswerOnNextPrompt,
+  webdriverChooseCancelOnVisibleConfirmation:
+    emitChooseCancelOnNextConfirmation,
+  webdriverChooseOkOnVisibleConfirmation: emitChooseOkOnNextConfirmation,
 }
 
 exporter.register.preprocessors(emitters)
